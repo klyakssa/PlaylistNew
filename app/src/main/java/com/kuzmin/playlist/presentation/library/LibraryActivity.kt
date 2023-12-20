68c17601e7a2e9cafe1284aecd4a8a0356427351
denis <denis.spb.2004@gmail.com>
2023-12-20 23:51:42 +0300

    Sprint 18 - Fragments № 1

--- app/src/main/java/com/kuzmin/playlist/presentation/library/LibraryActivity.kt
+++ app/src/main/java/com/kuzmin/playlist/presentation/library/LibraryActivity.kt
@@ -2,11 +2,39 @@ package com.kuzmin.playlist.presentation.library
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.tabs.TabLayoutMediator
 import com.kuzmin.playlist.R
+import com.kuzmin.playlist.databinding.ActivityLibraryBinding
 
 class LibraryActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLibraryBinding
+
+    private lateinit var tabMediator: TabLayoutMediator
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_library)
+        binding = ActivityLibraryBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.back.setOnClickListener {
+            finish()
+        }
+
+        binding.viewPager.adapter = LibraryViewPagerAdapter(supportFragmentManager,
+            lifecycle)
+
+        tabMediator = TabLayoutMediator(binding.tabLayout, binding.viewPager) { tab, position ->
+            when(position) {
+                0 -> tab.text = getString(R.string.favorites)
+                1 -> tab.text = getString(R.string.playlists)
+            }
+        }
+        tabMediator.attach()
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        tabMediator.detach()
     }
 }
